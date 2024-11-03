7dbccde8fdc14cb6586185bd502b9eab1377f016
Alan Emeršić <alan.emersic@gmail.com>
2024-11-03 19:47:29 +0100

    Domain models

---
+++ src/SettlementManager.Domain/Settlements/Country.cs
@@ -0,0 +1,10 @@
+﻿using SettlementManager.Domain.Common;
+
+namespace SettlementManager.Domain.Settlements;
+
+public sealed class Country : VersionedEntity
+{
+    public string Name { get; init; } = null!;
+
+    public IReadOnlyCollection<Settlement> Settlements { get; init; } = null!;
+}
---
+++ src/SettlementManager.Domain/Settlements/Settlement.cs
@@ -0,0 +1,12 @@
+﻿using SettlementManager.Domain.Common;
+
+namespace SettlementManager.Domain.Settlements;
+
+public sealed class Settlement : VersionedEntity
+{
+    public int CountryId { get; init; }
+    public string Name { get; init; } = null!;
+    public string PostalCode { get; init; } = null!;
+
+    public Country Country { get; init; } = null!;
+}
