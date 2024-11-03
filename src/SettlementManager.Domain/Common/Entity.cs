d4d322e19d647493312a2638586e495ffae9b6c5
AlanEmersic <47435221+AlanEmersic@users.noreply.github.com>
2024-11-03 19:49:28 +0100

    Domain models (#3)

---
+++ src/SettlementManager.Domain/Common/Entity.cs
@@ -0,0 +1,21 @@
+﻿namespace SettlementManager.Domain.Common;
+
+public abstract class Entity
+{
+    public int Id { get; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        return ((Entity)obj).Id == Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+}
---
+++ src/SettlementManager.Domain/Common/VersionedEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace SettlementManager.Domain.Common;
+
+public abstract class VersionedEntity : Entity
+{
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public byte[] RowVersion { get; set; } = [];
+}
