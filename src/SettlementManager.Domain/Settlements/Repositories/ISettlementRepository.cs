63f153e95083d513ed45b057dc94e6939fc049fd
Alan Emeršić <alan.emersic@gmail.com>
2024-11-03 21:31:01 +0100

    Domain model interfaces

---
+++ src/SettlementManager.Domain/Settlements/Repositories/ISettlementRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace SettlementManager.Domain.Settlements.Repositories;
+
+public interface ISettlementRepository
+{
+    Task<Settlement?> GetByIdAsync(int id);
+    Task AddAsync(Settlement settlement);
+    Task UpdateAsync(Settlement settlement);
+    Task DeleteAsync(Settlement settlement);
+}
