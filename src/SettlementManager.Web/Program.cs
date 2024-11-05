000557989a095c13fe75cce1ae0cc2b43e46e4d7
Alan Emeršić <alan.emersic@gmail.com>
2024-11-05 18:05:29 +0100

    Remove unused code

--- src/SettlementManager.Web/Program.cs
+++ src/SettlementManager.Web/Program.cs
@@ -1,24 +1,12 @@
-using SettlementManager.Application;
-using SettlementManager.Domain;
-using SettlementManager.Infrastructure;
 using SettlementManager.Web;
 using SettlementManager.Web.Components;
-using SettlementManager.Web.Services.Settlements;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.Services
-    // .AddDomain()
-    // .AddApplication()
-    // .AddInfrastructure(builder.Configuration)
-    .AddWeb();
+builder.Services.AddWeb();
 
 WebApplication app = builder.Build();
 
-// app.UseInfrastructure();
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 app.UseAntiforgery();
 app
     .MapRazorComponents<App>()
