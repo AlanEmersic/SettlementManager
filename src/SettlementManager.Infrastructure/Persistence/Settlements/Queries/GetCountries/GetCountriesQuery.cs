4e4847bebc2223737efa17d3e169257f205af9af
AlanEmersic <47435221+AlanEmersic@users.noreply.github.com>
2024-11-06 12:47:47 +0100

    Get countries api (#12)

---
+++ src/SettlementManager.Infrastructure/Persistence/Settlements/Queries/GetCountries/GetCountriesQuery.cs
@@ -0,0 +1,7 @@
+﻿using ErrorOr;
+using MediatR;
+using SettlementManager.Application.Settlements.DTO;
+
+namespace SettlementManager.Infrastructure.Persistence.Settlements.Queries.GetCountries;
+
+public sealed record GetCountriesQuery : IRequest<ErrorOr<IReadOnlyList<CountryDto>>>;
---
+++ src/SettlementManager.Infrastructure/Persistence/Settlements/Queries/GetCountries/GetCountriesQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using ErrorOr;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SettlementManager.Application.Settlements.DTO;
+using SettlementManager.Application.Settlements.Mappings;
+using SettlementManager.Infrastructure.Persistence.Database;
+
+namespace SettlementManager.Infrastructure.Persistence.Settlements.Queries.GetCountries;
+
+internal sealed class GetCountriesQueryHandler : IRequestHandler<GetCountriesQuery, ErrorOr<IReadOnlyList<CountryDto>>>
+{
+    private readonly SettlementManagerDbContext dbContext;
+
+    public GetCountriesQueryHandler(SettlementManagerDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async Task<ErrorOr<IReadOnlyList<CountryDto>>> Handle(GetCountriesQuery query, CancellationToken cancellationToken)
+    {
+        List<CountryDto> countries = await dbContext
+            .Countries
+            .AsNoTracking()
+            .Select(x => x.MapToDto())
+            .ToListAsync(cancellationToken);
+
+        return countries;
+    }
+}
