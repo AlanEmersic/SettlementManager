2b84f6a722ed3d76f7eb6d74610420210e89d603
AlanEmersic <47435221+AlanEmersic@users.noreply.github.com>
2024-11-13 18:00:03 +0100

    Remove unused  country repository (#19)

--- src/SettlementManager.Domain/Settlements/Repositories/ICountryRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SettlementManager.Domain.Settlements.Repositories;
-
-public interface ICountryRepository
-{
-    Task<Country?> GetByIdAsync(int id);
-    Task AddAsync(Country country);
-    Task UpdateAsync(Country country);
-    Task DeleteAsync(Country country);
-}
