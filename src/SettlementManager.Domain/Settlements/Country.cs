36241546308def2cc01a0d0f8b23efe4ad28ec7f
Alan Emeršić <alan.emersic@gmail.com>
2024-11-18 14:33:55 +0100

    Use required keyword in domain models

--- src/SettlementManager.Domain/Settlements/Country.cs
+++ src/SettlementManager.Domain/Settlements/Country.cs
@@ -4,7 +4,7 @@ namespace SettlementManager.Domain.Settlements;
 
 public sealed class Country : VersionedEntity
 {
-    public string Name { get; init; } = null!;
+    public required string Name { get; init; }
 
     public IReadOnlyCollection<Settlement> Settlements { get; init; } = null!;
 }
